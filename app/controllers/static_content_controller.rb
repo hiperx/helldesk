6f647a032ce7589a72c8dbd22ac7debb9fd9e1fd
Your Name <you@example.com>
2016-03-14 17:26:07 +0100

    Pierwszy dzien done

--- app/controllers/static_content_controller.rb
+++ app/controllers/static_content_controller.rb
@@ -1,10 +1,12 @@
 class StaticContentController < ApplicationController
   def help
+    @title='Help Me'
   end
 
   def about
   end
 
   def start
+    @title='From controller'
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def provide_title(subtitle='')
+    hell= 'Helldesk' 
+    unless @title.nil?
+      subtitle=@title
+    end
+    if subtitle.empty?
+      hell
+    else
+      hell + " | " + subtitle
+    end
+  end
 end
