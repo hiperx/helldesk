697eeb1e83d2657e7c37e76826e53f42588728ba
Your Name <you@example.com>
2016-03-15 17:31:26 +0100

    Day 2 done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,49 @@
+require 'digest/sha2'
+
 class User < ActiveRecord::Base
+  validates :name, :presence => true, :uniqueness => true
+  validates :name, length: {in: 5..15}
+  validates :password, :confirmation => true
+
+  validate :password_must_be
+  validate :password_lenght
+
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  def password=(password)
+    @password=password
+    puts 'punkt 1' #sprawdzanie - przy wykonywaniu wyzuca na ekran
+    if @password.present?
+      # tu bedzie kodowanie
+      # puts ' punkt 2'
+      self.hash_password = User.encrypt_password(@password)
+    end
+  end
+
+def User.encrypt_password(password)
+  Digest::SHA2.hexdigest password
+end
+
+def User.authenticate(name,password)
+  if u = User.find_by_name(name)
+    if u.hash_password == User.encrypt_password(password)
+      u
+    end
+  end
+end
+
+private
+
+  def password_lenght
+    if @password.present?
+      errors.add(:password, 'is to short') if @password.size < 5
+    end
+  end
+
+  def password_must_be
+      #self.hash_password.empty ?
+      errors.add(:password, 'Can by empty') unless self.hash_password.present?
+  end
+
 end
